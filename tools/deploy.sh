9b6f94f283ffde15c2860eaa12a91a3fa3e2cd49
Allajah <r1o.ryoma.abe@gmail.com>
2017-12-08 18:05:06 +0900

    Create a script to deploy to GitHub Pages

---
+++ tools/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hash=`git rev-parse HEAD`
+commit_message="Release: $hash"
+
+echo $commit_message
+npm run build
+git add -f ./dist
+git commit -m "$commit_message"s
+git subtree push --prefix dist origin gh-pages
