70d24eea94a73d194bd4748294c1f45a40116502
arayaryoma <r1o.ryoma.abe@gmail.com>
2018-06-09 23:29:16 +0900

    Fix script to depoly to gh-pages

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -8,3 +8,4 @@ npm run build
 git add -f ./dist
 git commit -m "$commit_message"s
 git subtree push --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist master`:gh-pages --force
